dc27ae0e3c1f5c7b539e96dbc0d1814c78cea415
Julia Vila Guilera <65185919+juliavigu@users.noreply.github.com>
2020-07-22 13:27:07 +0200

    final update

--- plot3.R
+++ plot3.R
@@ -16,6 +16,7 @@ png("plot3.png") #save to png file
 plot(df1$datetime,df1$Sub_metering_1, col="black", type="l", ylab="Energy sub metering", xlab="")
 lines(df1$datetime,df1$Sub_metering_2, col="red", type="l")
 lines(df1$datetime, df1$Sub_metering_3, col="blue", type="l")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 dev.off() #close connection
 
 
