056fe16cc5c00de4f79d1b90f838d83bd361b764
Julia Vila Guilera <65185919+juliavigu@users.noreply.github.com>
2020-07-22 13:26:47 +0200

    final update

--- plot4.R
+++ plot4.R
@@ -22,9 +22,9 @@ plot(df1$datetime,df1$Voltage, type="l", ylab="Voltage", xlab="datetime")
 plot(df1$datetime,df1$Sub_metering_1, col="black", type="l", ylab="Energy sub metering", xlab="") 
 lines(df1$datetime,df1$Sub_metering_2, col="red", type="l")
 lines(df1$datetime, df1$Sub_metering_3, col="blue", type="l")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 #plot bottom right
 plot(df1$datetime,df1$Global_reactive_power, type="l", ylab="Global_reactive_power", xlab="datetime")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 dev.off() #close connection
 
 
